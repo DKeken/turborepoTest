ff7f887734f7e577ad74ab2d9abf64643bb9c9df
Daniel <dshovkunov@intas-company.com>
2023-10-04 10:51:11 +0500

    changes

---
+++ apps/trends/src/index.ts
@@ -0,0 +1,1 @@
+export { default as TrendsMain } from "./App";
--- images/admin/src/App.tsx
+++ images/admin/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Button } from "./components/Button";
+import { TrendsMain } from "trends";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button />
+        <TrendsMain />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
